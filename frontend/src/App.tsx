c76dfb4bf3e24c75d4e32ad795ebc4e83bad35f0
jekattack <jan.erik.kraus@t-online.de>
2022-06-24 12:06:49 +0200

    unused imports removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Header from "./components/Header";
-import Form from "./components/Form";
-import Gallery from "./components/Gallery";
-import { Link } from "react-router-dom";
 import MainPage from "./routes/MainPage";
 import EditPage from "./routes/EditPage";
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-    BrowserRouter,
-    Routes,
-    Route} from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MainPage from "./routes/MainPage";
-import EditPage from "./routes/EditPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
